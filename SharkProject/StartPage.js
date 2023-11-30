d9d79d6f1c082f62660634523ba90aca3bf79116
himalsait <120237214+himalsait@users.noreply.github.com>
2023-11-30 12:38:31 -0700

    Main page
    I have completed the main page

--- SharkProject/StartPage.js
+++ SharkProject/StartPage.js
@@ -1,23 +1,27 @@
-// StartPage.js
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-const StartPage = ({ navigation }) => {
-  // Add your button handlers and logic here
-  
+const StartPage = () => {
+  const handlePlayPress = () => {
+    // Insert your play button logic here
+    console.log('Play button pressed');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>SHARK CHASE</Text>
-      <TouchableOpacity style={styles.playButton} onPress={() => {/* Play button logic */}}>
-        <Ionicons name="play-circle" size={64} color="white" />
-      </TouchableOpacity>
-      <View style={styles.bottomControls}>
-        <TouchableOpacity onPress={() => {/* Volume logic */}}>
-          <Ionicons name="volume-high" size={32} color="white" />
+      <View style={styles.content}>
+        <Text style={styles.title}>SHARK CHASE</Text>
+        <TouchableOpacity style={styles.playButton} onPress={handlePlayPress}>
+          <Ionicons name="play-circle" size={64} color="black" />
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {/* Settings logic */}}>
-          <Ionicons name="settings" size={32} color="white" />
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={() => console.log('Volume pressed')}>
+          <Ionicons name="volume-high" size={32} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log('Settings pressed')}>
+          <Ionicons name="settings" size={32} color="black" />
         </TouchableOpacity>
       </View>
     </View>
@@ -25,7 +29,28 @@ const StartPage = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  // Add your styles here
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  playButton: {
+    // Define styles for your play button if needed
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingBottom: 20,
+  },
+  // ... other styles
 });
 
 export default StartPage;
